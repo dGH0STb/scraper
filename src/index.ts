e79d5c42228b1c3a8cd7cd0b083761b7d4e1d91c
Dhulfiqar Alkhursan~ <dhulfiqaralkhursan@gmail.com>
2025-04-20 16:34:28 +0300

    Add initial setup for Puppeteer scraper project
    This commit includes configuration files for TypeScript, Serverless, Docker, and BuildSpec, as well as a basic index file and deployment script. These changes establish the foundational environment for building, deploying, and managing the Puppeteer-based scraping application.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,69 @@
-console.log('Happy developing ✨')
+import { Callback, Context, Handler } from "aws-lambda";
+import { Browser, Page, PuppeteerLaunchOptions } from "puppeteer";
+import { PuppeteerExtra } from "puppeteer-extra";
+
+interface ExampleEvent {
+    //type your event here if desired
+}
+
+export const handler: Handler = async (
+    event: ExampleEvent,
+    context: Context,
+    callback: Callback
+): Promise<any> => {
+    try {
+        console.log("event:", event);
+        const puppeteer: PuppeteerExtra = require("puppeteer-extra");
+        const stealthPlugin = require("puppeteer-extra-plugin-stealth");
+        puppeteer.use(stealthPlugin());
+
+        // const proxyPlugin = require("puppeteer-extra-plugin-proxy");
+        // puppeteer.use(
+        //   proxyPlugin({
+        //     address: "pr.oxylabs.io",
+        //     port: 7777,
+        //     credentials: {
+        //       username: "customer-someUsername-cc-US",
+        //       password: "somePassword",
+        //     },
+        //   })
+        // );
+
+        const launchOptions: PuppeteerLaunchOptions = context.functionName
+            ? {
+                headless: true,
+                executablePath: puppeteer.executablePath(),
+                args: [
+                    "--no-sandbox",
+                    "--disable-setuid-sandbox",
+                    "--disable-dev-shm-usage",
+                    "--disable-gpu",
+                    "--single-process",
+                ],
+            }
+            : {
+                headless: false,
+                executablePath: puppeteer.executablePath(),
+            };
+
+        const browser: Browser = await puppeteer.launch(launchOptions);
+        const page: Page = await browser.newPage();
+        await page.goto("https://www.example.com");
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        console.log(await page.content());
+        await browser.close();
+    } catch (e) {
+        console.log("Error in Lambda Handler:", e);
+        return e;
+    }
+};
+// Test - npx ts-node index.ts
+(async () => {
+    try {
+        const event: ExampleEvent = {};
+        //@ts-ignore
+        await handler(event, {}, () => {});
+    } catch (e) {
+        console.log("Error in Lambda Handler:", e);
+    }
+})();
