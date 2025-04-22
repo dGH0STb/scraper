0e8b9ba003ca66fcbd24b03b91deacc5bc8e12f7
Dhulfiqar Alkhursan~ <dhulfiqaralkhursan@gmail.com>
2025-04-22 16:45:14 +0300

    Add AWS ECR setup and CodeBuild policy configuration
    Set up AWS ECR repository check and automatic creation in deploy script. Configure AWS region in buildspec.yml and add a new IAM policy for CodeBuild to manage ECR operations. Ensure deployment process halts on errors for reliability with `set -e`.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+set -e  # Exit immediately if any command fails
 
 echo "command - npx tsc"
 npx tsc
 
 echo "command - aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 429744660197.dkr.ecr.us-west-2.amazonaws.com"
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 429744660197.dkr.ecr.us-west-2.amazonaws.com
+
+# Check if the repository exists, create it if it doesn't
+aws ecr describe-repositories --repository-names scraper --region us-west-2 || aws ecr create-repository --repository-name scraper --region us-west-2
 
 echo "command - docker build --platform linux/amd64 --progress=plain -t scraper ."
 docker build --platform linux/amd64 --progress=plain -t scraper .
